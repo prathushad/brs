fca4d2edc5fcc1c11a86c5a512bd2b7ea2d50b3f
Prathusha <prathusha.desu@gmail.com>
2015-04-30 01:09:40 -0700

    Improved login home

--- src/main/java/com/brs/controller/BRSHomeController.java
+++ src/main/java/com/brs/controller/BRSHomeController.java
@@ -2,6 +2,7 @@ package com.brs.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -10,15 +11,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.brs.model.User;
+import com.brs.service.book.BookManagementService;
 
 @Controller
 public class BRSHomeController {
+	
+	@Autowired
+	private BookManagementService bookManagementService;
 
 	@RequestMapping(value = "/portal", method = RequestMethod.GET)
 	public String getList(HttpServletRequest httpRequest, Model model) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		User user = (User) auth.getPrincipal();
+		
+		int numberOfBooksCheckedOut = bookManagementService.findBooksCheckedOutByUser(user.getId()).size();
+		int numberOfBooksOnHold = bookManagementService.findBooksOnHoldForUser(user.getId()).size();
+		
 		model.addAttribute("displayname", user.getFullName());
+		model.addAttribute("checkouts", numberOfBooksCheckedOut);
+		model.addAttribute("holds", numberOfBooksOnHold);
+		
 		return "portal";
 	}
 
--- src/main/webapp/WEB-INF/views/account/portal.jsp
+++ src/main/webapp/WEB-INF/views/account/portal.jsp
@@ -5,6 +5,22 @@
 	<div class="row-fluid" style="height: 400px;">
 		<%@ include file="../layouts/sidebar.jsp"%>
 		<div class="span9 dashboard-rightpanel margin30-b" id="rightpanel">
+
+			<div style="text-align: center;padding-top:70px;">Get ready to access a wide variety of collection of books!</div> 
+			<br />
+			<c:if test="${checkouts == 0}">
+					<div class="alert alert-danger" style="text-align: center;">You have no books checked out.</div>
+			</c:if>
+			<c:if test="${checkouts > 0}">
+					<div class="alert alert-info" style="text-align: center;">You have <c:out value="${checkouts}"/> book(s) checked out.</div>
+			</c:if>
+			<c:if test="${holds == 0}">
+					<div class="alert alert-danger" style="text-align: center;">You have no books on hold.</div>
+			</c:if>
+			<c:if test="${holds > 0}">
+					<div class="alert alert-info" style="text-align: center;">You have <c:out value="${holds}"/> book(s) on hold.</div>
+			</c:if>
+			
 		</div>
 	</div>
 
